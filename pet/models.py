6f2ddb452bf8a59233772acbe02d800b91b2db24
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-06-05 01:04:37 -0300

    criando a tabela 'Pet'

--- pet/models.py
+++ pet/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Pet(models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    historia = models.TextField(blank=False, null=False)
+    foto = models.URLField(blank=False, null=False)
