01173b7c48a3a63bf824c55cf5242141b330a26d
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-06-06 15:48:14 -0300

    criando a API para listar pets

--- pet/views.py
+++ pet/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Pet
+from .serializers import PetSerializer
+
+
+class PetList(APIView):
+    def get(self, request, format=None):
+        pets = Pet.objects.all()
+        serializer = PetSerializer(pets, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
