95a3c6c48a8e471feeeee9a9d8ac720c7fe757e7
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-06-09 00:16:42 -0300

    adicionando action de listagem de Adocoes

--- adocao/views.py
+++ adocao/views.py
@@ -1,12 +1,18 @@
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
 from .email_service import enviar_email_confirmacao
+from .models import Adocao
 from .serializers import AdocaoSerializer
 
 
 class AdocaoList(APIView):
+    def get(self, request, format=None):
+        adocoes = Adocao.objects.all()
+        serializer = AdocaoSerializer(adocoes, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
+
     def post(self, request, format=None):
         serializer = AdocaoSerializer(data=request.data)
 
@@ -16,6 +22,9 @@ class AdocaoList(APIView):
             return Response(serializer.data, status=HTTP_201_CREATED)
 
         return Response(
-            {"errors": serializer.errors, "message": "Houveram erros de validação."},
+            {
+                "errors": serializer.errors,
+                "message": "Houveram erros de validação.",
+            },
             status=HTTP_400_BAD_REQUEST,
         )
